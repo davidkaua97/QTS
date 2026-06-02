939cb3d95ed0f529dff712e5ef7b09e069bc83cf
davidkaua97 <dv9596032@gmail.com>
2026-06-01 21:47:28 -0300

    final

--- script.js
+++ script.js
@@ -15,5 +15,87 @@ function maior(a, b) {
   }
 }
 
-module.exports = { somar, mensagemBoasVindas, maior };
+function parImpar(a, b) {
+  if (a % 2 == 0 && b % 2 != 0) {
+    return `somente o numero ${a} é par e ${b} é impar`
+  }
+  else if(b % 2 == 0 && a % 2 != 0) {
+    return `somente o numero${b} é par e ${a} é impar`
+  } else if (a % 2 == 0 && b % 2 == 0) {
+    return 'ambos são par'
+  }
+  else {
+    return 'ambos são impar'
+  }
+}
+
+function verificarVoto(idade) {
+  if (idade < 16) {
+    return "Não pode votar";
+  } else if (idade < 18) {
+    return "Voto opcional";
+  } else if (idade < 70) {
+    return "Voto obrigatório";
+  } else {
+    return "Voto opcional";
+  }
+}
+
+function bonus(salario) {
+  if(salario<2000) {
+    return salario * 0.20;
+  }
+  else if (salario <= 5000) {
+    return salario * 0.10;
+  }
+  else {
+    return salario * 0.05;
+  }
+}
+
+function podeDirigir(idade) {
+  if(idade > 18) {
+    return "pode dirigir"
+  }
+  else {
+    return 'não pode dirigir'
+  }
+}
+
+function semestre(mes) {
+  switch (mes.toLowerCase()) {
+    case "janeiro":
+    case "fevereiro":
+    case "março":
+    case "abril":
+    case "maio":
+    case "junho":
+      return "primeiro semestre";
+
+    case "julho":
+    case "agosto":
+    case "setembro":
+    case "outubro":
+    case "novembro":
+    case "dezembro":
+      return "segundo semestre";
+
+    default:
+      return "mes invalido";
+  }
+}
+
+function vogalConsoante(l) {
+  l = l.toLowerCase();
+
+  if (l === "a" || l === "e" || l === "i" || l === "o" || l === "u") {
+    return "vogal";
+  } else {
+    return "consoante";
+  }
+}
+
+
+
+module.exports = { somar, mensagemBoasVindas, maior, parImpar, verificarVoto, bonus, podeDirigir, semestre, vogalConsoante};
 // export default { somar, mensagemBoasVindas };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { somar, mensagemBoasVindas, maior } = require('./script');
+const { somar, mensagemBoasVindas, maior, parImpar, verificarVoto, bonus, podeDirigir, semestre, vogalConsoante} = require('./script');
 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
@@ -12,3 +12,26 @@ test('maior', () => {
   expect(maior(5, 3)).toBe(5);
 });
 
+test('par ou impar', () => {
+  expect(parImpar(2, 2)).toBe('ambos são par')
+}); 
+
+test('votar', () => {
+  expect(verificarVoto(75)).toBe("Voto opcional")
+})
+
+test('bonus', () => {
+  expect(bonus(3000)).toBe(3000 * 0.10)
+})
+
+test('dirigir', () => {
+  expect(podeDirigir(16)).toBe('não pode dirigir')
+})
+
+test('semestre', () => {
+  expect(semestre('março')).toBe('primeiro semestre')
+})
+
+test('vogal ou consoante', () => {
+  expect(vogalConsoante('a')).toBe('vogal')
+})
